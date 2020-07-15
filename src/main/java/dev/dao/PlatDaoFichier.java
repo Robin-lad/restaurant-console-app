6ea60123bf3ddc356a0e8a71eb25891bb4909c12
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-07-15 16:34:15 +0200

    3.4

--- src/main/java/dev/dao/PlatDaoFichier.java
+++ src/main/java/dev/dao/PlatDaoFichier.java
@@ -23,7 +23,7 @@ public class PlatDaoFichier implements IPlatDao {
 
     private String fichierStockage;
 
-    public PlatDaoFichier(@Value("${path}") String fichierStockage) {
+    public PlatDaoFichier(@Value("${fichier.stockage}") String fichierStockage) {
         this.fichierStockage = fichierStockage;
         if (!Files.exists(Paths.get(this.fichierStockage))) {
             try {
---
+++ src/test/java/dev/dao/PlatDaoFichierTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package dev.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import dev.entite.Plat;
+
+/**
+ * @author robin
+ *
+ */
+@SpringJUnitConfig(classes = { PlatDaoFichier.class })
+@TestPropertySource("classpath:test.properties")
+public class PlatDaoFichierTest {
+	
+	@Autowired
+	PlatDaoFichier dao;
+	
+	@Test
+	void ajouterPlatTest() {
+		dao.ajouterPlat("saucisses", 15000);
+		assertThat(dao.listerPlats()).extracting(Plat::getNom).contains("saucisses");
+		assertThat(dao.listerPlats()).extracting(Plat::getPrixEnCentimesEuros).contains(15000);
+		
+		assertThat(dao.listerPlats()).containsExactly(new Plat("saucisses", 15000));
+		
+		assertThat(dao.listerPlats()).size().isOne();
+	}
+}
